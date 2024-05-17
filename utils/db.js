e437911816bbf47b7e8bf7242b1674f66e1a350e
ngarukaalx <hiramkabuagi@gmail.com>
2024-05-17 16:04:30 +0000

    fix lint

--- utils/db.js
+++ utils/db.js
@@ -1,205 +1,208 @@
 import mongodb from 'mongodb';
 
 class DBClient {
-	constructor() {
-		const host = process.env.DB_HOST || '127.0.0.1';
-		const port = process.env.DB_PORT || 27017;
-		const database = process.env.DB_DATABASE || 'files_manager';
-		
-		// connection url
-		const url = `mongodb://${host}:${port}`;
-		this.client = new mongodb.MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
-		this.client.connect().then(() => {
-			this.db = this.client.db(`${database}`);
-		}).catch((err) => {
-			console.log(err);
-		});
-	}
+  constructor() {
+    const host = process.env.DB_HOST || '127.0.0.1';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
 
-	// a function isAlive that returns true when the connection
-	// to MongoDB is a success otherwise, false
-	isAlive() {
-		return this.client.isConnected();
-	}
+    // connection url
+    const url = `mongodb://${host}:${port}`;
+    this.client = new mongodb.MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+    this.client.connect().then(() => {
+      this.db = this.client.db(`${database}`);
+    }).catch((err) => {
+      console.log(err);
+    });
+  }
 
-	// an asynchronous function nbUsers that returns the number
-	// of documents in the collection users
-	async nbUsers() {
-		try {
-			const usersCount = await this.db.collection('users').countDocuments();
-		        return usersCount;
-		} catch (error) {
-			console.log('Error count users: ', error.message);
-		}
-	}
-	// an asynchronous function nbFiles that returns the number of
-	// documents in the collection files
-	async nbFiles() {
-		try {
-			const filesCount = await this.db.collection('files').countDocuments();
-		        return filesCount;
-		} catch (error) {
-			console.log('Error count files: ', error.message);
-		}
-	}
-	// a fuction to create a user
-	async newUser(email, password) {
-		try {
-			// construct a query
-			const user = {
-				email: email,
-				password: password
-			};
+  // a function isAlive that returns true when the connection
+  // to MongoDB is a success otherwise, false
+  isAlive() {
+    return this.client.isConnected();
+  }
 
-			const result = await this.db.collection('users').insertOne(user);
-			// return the id of the inserted user
-			return result.insertedId;
-		} catch (error) {
-			console.log('Error creating user: ', error.message);
-			return null;
-		}
-	}
+  // an asynchronous function nbUsers that returns the number
+  // of documents in the collection users
+  async nbUsers() {
+    try {
+      const usersCount = await this.db.collection('users').countDocuments();
+      return usersCount;
+    } catch (error) {
+      console.log('Error count users: ', error.message);
+      return null;
+    }
+  }
 
-	// function to create a newfile
-	async newfile(data) {
-		try {
-			const result = await this.db.collection('files').insertOne(data);
-			const insertedFile = result.ops[0];
-			return insertedFile;
-		} catch (error) {
-			console.log('Error creating file: ', error.message);
-			return null;
-		}
-	}
+  // an asynchronous function nbFiles that returns the number of
+  // documents in the collection files
+  async nbFiles() {
+    try {
+      const filesCount = await this.db.collection('files').countDocuments();
+      return filesCount;
+    } catch (error) {
+      console.log('Error count files: ', error.message);
+      return null;
+    }
+  }
 
-	// fuction to find a folder by parent id
-	async parentid(parentId) {
-		try {
-			// construct a query
-			const query = {
-				_id: parentId
-			};
-			const result = await this.db.collection('files').findOne(query);
-			return result;
-		} catch (error) {
-			console.log('Error fetching file: ', error.message);
-			return null;
-		}
-	}
+  // a fuction to create a user
+  async newUser(email, password) {
+    try {
+      // construct a query
+      const user = {
+        email,
+        password,
+      };
 
-	// add a new field
-	async addnewfield(folderid, fieldname, value) {
-		try {
-			const query = {
-				_id: folderid
-			};
+      const result = await this.db.collection('users').insertOne(user);
+      // return the id of the inserted user
+      return result.insertedId;
+    } catch (error) {
+      console.log('Error creating user: ', error.message);
+      return null;
+    }
+  }
 
-			const update = {
-				$set: { [fieldname]: value}
-			};
+  // function to create a newfile
+  async newfile(data) {
+    try {
+      const result = await this.db.collection('files').insertOne(data);
+      const insertedFile = result.ops[0];
+      return insertedFile;
+    } catch (error) {
+      console.log('Error creating file: ', error.message);
+      return null;
+    }
+  }
 
-			const result = await this.db.collection('files').updateOne(query, update);
-			return result;
-		} catch (error) {
-			console.log('Error in update: ', error.message);
-			return null;
-		}
-	}
+  // fuction to find a folder by parent id
+  async parentid(parentId) {
+    try {
+      // construct a query
+      const query = {
+        _id: parentId,
+      };
+      const result = await this.db.collection('files').findOne(query);
+      return result;
+    } catch (error) {
+      console.log('Error fetching file: ', error.message);
+      return null;
+    }
+  }
 
-	// update a field
-	async publishUpublish(fileid, value) {
-		try {
-			const query = {
-				_id: fileid
-			};
+  // add a new field
+  async addnewfield(folderid, fieldname, value) {
+    try {
+      const query = {
+        _id: folderid,
+      };
 
-			const update = {
-				$set: { ['isPublic']: value }
-			};
-			// to return the modified
-			const options = {
-				returnOriginal: false
-			};
+      const update = {
+        $set: { [fieldname]: value },
+      };
 
-			const result =  await this.db.collection('files').findOneAndUpdate(query, update, options);
-			return result.value;
-		} catch (error) {
-			console.error('Error in update: ', error.message);
-			return null;
-		}
-	}
+      const result = await this.db.collection('files').updateOne(query, update);
+      return result;
+    } catch (error) {
+      console.log('Error in update: ', error.message);
+      return null;
+    }
+  }
 
-	// find files by parent id return by limit(pagination)
-	async parentidPagination(parentid, userid, page, limit) {
-		try {
-			// calculete skip
-			const skip = page * limit;
+  // update a field
+  async publishUpublish(fileid, value) {
+    try {
+      const query = {
+        _id: fileid,
+      };
 
-			// construct query
-			const query = {
-				$and: [
-					{ parentId: parentid },
-					{ userId: userid }
-				]
-			};
-			
-			// find files matching query
-			const files = await this.db.collection('files').find(query).skip(skip).limit(limit).toArray();
-			return files;
-		} catch (error) {
-			console.error('Error seraching: ', error.message);
-			return [];
-		}
+      const update = {
+        $set: { isPublic: value },
+      };
+      // to return the modified
+      const options = {
+        returnOriginal: false,
+      };
 
-	}
+      const result = await this.db.collection('files').findOneAndUpdate(query, update, options);
+      return result.value;
+    } catch (error) {
+      console.error('Error in update: ', error.message);
+      return null;
+    }
+  }
 
-	// fuction to find a user by email
-	async findUser(email) {
-		try {
-			// construct a query
-			const query = {
-				email: email
-			};
-			const result = await this.db.collection('users').findOne(query);
-			return result;
-		} catch (error) {
-			console.log('Error finding user: ', error.message);
-			return null;
-		}
+  // find files by parent id return by limit(pagination)
+  async parentidPagination(parentid, userid, page, limit) {
+    try {
+      // calculete skip
+      const skip = page * limit;
 
-	}
+      // construct query
+      const query = {
+        $and: [
+          { parentId: parentid },
+          { userId: userid },
+        ],
+      };
 
-	// fuction to find user by email and pass
-	async finduserEp(email, pass) {
-		try {
-			// construct a query
-			const query = {
-				email: email,
-				password: pass
-			};
-			const result = await this.db.collection('users').findOne(query);
-			return result;
-		} catch (error) {
-			console.log('Error finding user: ', error.message);
-			return null;
-		}
-	}
+      // find files matching query
+      const files = await this.db.collection('files').find(query).skip(skip).limit(limit)
+        .toArray();
+      return files;
+    } catch (error) {
+      console.error('Error seraching: ', error.message);
+      return [];
+    }
+  }
 
-	// fuction to find a user by id
-	async userByid(id) {
-		try {
-			// construct a query
-			const query = {
-				_id: id
-			};
-			const result = await this.db.collection('users').findOne(query);
-			return result
-		} catch (error) {
-			console.log('Error finding user: ', error.message);
-			return null;
-		}
-	}
-};
+  // fuction to find a user by email
+  async findUser(email) {
+    try {
+      // construct a query
+      const query = {
+        email,
+      };
+      const result = await this.db.collection('users').findOne(query);
+      return result;
+    } catch (error) {
+      console.log('Error finding user: ', error.message);
+      return null;
+    }
+  }
+
+  // fuction to find user by email and pass
+  async finduserEp(email, pass) {
+    try {
+      // construct a query
+      const query = {
+        email,
+        password: pass,
+      };
+      const result = await this.db.collection('users').findOne(query);
+      return result;
+    } catch (error) {
+      console.log('Error finding user: ', error.message);
+      return null;
+    }
+  }
+
+  // fuction to find a user by id
+  async userByid(id) {
+    try {
+      // construct a query
+      const query = {
+        _id: id,
+      };
+      const result = await this.db.collection('users').findOne(query);
+      return result;
+    } catch (error) {
+      console.log('Error finding user: ', error.message);
+      return null;
+    }
+  }
+}
 
 // create and export an instance of DBClient called dbClient
 const dbClient = new DBClient();
--- utils/redis.js
+++ utils/redis.js
@@ -3,64 +3,64 @@ import util from 'util';
 
 // class redisclient
 class RedisClient {
-	constructor() {
-		this.client = redis.createClient();
-		this.client.on('error', (err) => {
-			console.log('Error: ', err.message);
-		});
-	}
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (err) => {
+      console.log('Error: ', err.message);
+    });
+  }
 
-	// returns true when the connection to Redis
-	// is a success otherwise, false
-	isAlive() {
-		return this.client.connected;
-	}
+  // returns true when the connection to Redis
+  // is a success otherwise, false
+  isAlive() {
+    return this.client.connected;
+  }
 
-	// an asynchronous function get that takes a string
-	// key as argument and returns the Redis value stored
-	// for this key
-	async get(key) {
-		const promisify = util.promisify;
-		const getAsync = promisify(this.client.get).bind(this.client);
+  // an asynchronous function get that takes a string
+  // key as argument and returns the Redis value stored
+  // for this key
+  async get(key) {
+    const { promisify } = util;
+    const getAsync = promisify(this.client.get).bind(this.client);
+    try {
+      const value = await getAsync(key);
+      return value;
+    } catch (err) {
+      return err.message;
+    }
+  }
 
-	        try {
-			const value = await getAsync(key);
-		        return value;
-	        } catch (err) {
-		        return err.message;
-		}
-	}
+  // an asynchronous function set that takes a string key, a value
+  // and a duration in second as arguments to store it in Redis
+  // (with an expiration set by the duration argument)
+  async set(key, value, duration) {
+    // promisify Redis client methods
+    const { promisify } = util;
+    const setAsync = promisify(this.client.set).bind(this.client);
+    const expireAsync = promisify(this.client.expire).bind(this.client);
 
-	// an asynchronous function set that takes a string key, a value
-	// and a duration in second as arguments to store it in Redis
-	// (with an expiration set by the duration argument)
-	async set(key, value, duration) {
-		// promisify Redis client methods
-		const promisify = util.promisify;
-		const setAsync = promisify(this.client.set).bind(this.client);
-		const expireAsync = promisify(this.client.expire).bind(this.client);
+    try {
+      // set the key with the value
+      await setAsync(key, value);
+      // set the expiration time in seconds
+      await expireAsync(key, duration);
+    } catch (err) {
+      console.log('Error setting', err.message);
+    }
+  }
 
-		try {
-			// set the key with the value
-			await setAsync(key, value);
-			// set the expiration time in seconds
-			await expireAsync(key, duration);
-		} catch (err) {
-			console.log('Error setting', err.message);
-		}
-	}
-	// an asynchronous function del that takes a string key as
-	// argument and remove the value in Redis for this key
-	async del(key) {
-		const promisify = util.promisify;
-		const delAsync = promisify(this.client.del).bind(this.client);
-		try {
-			// delete the value
-			await delAsync(key);
-		} catch (err) {
-			console.log('Error in deletion: ', err.message);
-		}
-	}
+  // an asynchronous function del that takes a string key as
+  // argument and remove the value in Redis for this key
+  async del(key) {
+    const { promisify } = util;
+    const delAsync = promisify(this.client.del).bind(this.client);
+    try {
+      // delete the value
+      await delAsync(key);
+    } catch (err) {
+      console.log('Error in deletion: ', err.message);
+    }
+  }
 }
 
 // create and export an instance of RedisClient called redisClient
